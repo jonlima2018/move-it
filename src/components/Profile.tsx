00927facc883bb20ed1bd7173a97a8811da95a1a
jonlima <jonathan_martins_lima@yahoo.com.br>
2021-03-08 22:07:08 -0300

    feat: add login context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
+import { LoginContext } from "../contexts/LoginContext";
 import styles from "../style/components/Profile.module.css";
 
 export function Profile() {
   const { level } = useContext(ChallengesContext);
+  const { name, avatar_url } = useContext(LoginContext);
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/jonlima2018.png" alt="Jonathan Martins" />
+      <img src={avatar_url} alt="Jonathan Martins" />
       <div>
-        <strong>Jonathan Martins</strong>
+        <strong>{name}</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level {level}
